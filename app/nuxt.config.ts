08b9af46282cdb18ff1b36c6215aa1a4b66afa0b
takyafumin <takyafumin@gmail.com>
2024-06-08 23:39:10 +0900

    フォルダ構成変更

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,6 +1,6 @@
-import colors from 'vuetify/es5/util/colors'
+import { Configuration } from '@nuxt/types'
 
-export default {
+const config: Configuration = {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - app',
@@ -16,6 +16,8 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
+
+  srcDir: './src',
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
@@ -46,25 +48,8 @@ export default {
     baseURL: '/',
   },
 
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
-    },
-  },
-
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 }
+
+export default config
